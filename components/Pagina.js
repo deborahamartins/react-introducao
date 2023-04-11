a10ca15cde96f88621089b3443e0e90ae6b9b9cd
Déborah Martins <deborahamartins29@gmail.com>
2023-04-11 19:14:41 -0300

    update

--- components/Pagina.js
+++ components/Pagina.js
@@ -13,8 +13,10 @@ const Pagina = (props) => {
           <h1>{props.titulo}</h1>
         </Container>
       </div>
-
+      <Container className='mb-5 pb-3'>
       {props.children}
+      </Container>
+      
       
       <div style={{width: '100%'}} className="bg-dark position-fixed bottom-0 py-3 text-white text-center">
         <p>Todos os direitos reservados</p>
